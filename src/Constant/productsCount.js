6c6571f200241a4803a96640286f8cfd4be16880
Jinny <ggab.challenge@gmail.com>
2024-11-09 01:26:02 +0900

    refactor: 상품 보기 출력 부분 객체를 이용하여 출력할 수 있게 변경

--- src/Constant/productsCount.js
+++ src/Constant/productsCount.js
@@ -1,39 +1,53 @@
 import { ParedProductsData } from '../Model/productsInPossessionData.js';
 
-const productAmount = new ParedProductsData().productsInPossession();
+const rawProductList = new ParedProductsData().productsInPossession();
+
+const PRODUCTS = [
+  { productName: rawProductList[0][0], price: 1000, regularStock: 10, promotionStock: 10, promotionType: '탄산2+1'}, // 콜라
+  { productName: rawProductList[2][0], price: 1000, regularStock: 8, promotionStock: 7, promotionType: '탄산2+1'}, // 사이다
+  { productName: rawProductList[4][0], price: 1800, regularStock: null, promotionStock: 9, promotionType: 'MD추천상품'}, // 오렌지주스
+  { productName: rawProductList[5][0], price: 1200, regularStock: null, promotionStock: 5, promotionType: '탄산2+1'}, // 탄산수
+  { productName: rawProductList[6][0], price: 500, regularStock: 10, promotionStock: null, promotionType: null}, // 물
+  { productName: rawProductList[7][0], price: 1500, regularStock: 6, promotionStock: null, promotionType: null}, // 비타민원터
+  { productName: rawProductList[8][0], price: 1500, regularStock: 5, promotionStock: 5, promotionType: '반짝할인'}, // 감자칩
+  { productName: rawProductList[10][0], price: 1200, regularStock: 5, promotionStock: 5, promotionType: 'MD추천상품'}, //초코바
+  { productName: rawProductList[12][0], price: 2000, regularStock: 5, promotionStock: null, promotionType: null}, // 에너지바
+  { productName: rawProductList[13][0], price: 6400, regularStock: 8, promotionStock: null, promotionType: null}, // 정식도시라ㅣㄱ
+  { productName: rawProductList[14][0], price: 1700, regularStock: 10, promotionStock: 1, promotionType: 'MD추천상품'}, //컵라면
+];
 
 const NORMAL_PRODUCT_AMOUNT = {
-  COKE: Number(productAmount[1][2]),
-  SPRITE: Number(productAmount[3][2]),
-  WATER: Number(productAmount[6][2]),
-  VITAMIN_WATER: Number(productAmount[7][2]),
-  POTATO_CHIP: Number(productAmount[9][2]),
-  CHOCOLATE_BAR: Number(productAmount[11][2]),
-  ENERGY_BAR: Number(productAmount[12][2]),
-  PACKED_LUNCH: Number(productAmount[13][2]),
-  CUP_RAMEN: Number(productAmount[15][2]),
+  COKE: Number(rawProductList[1][2]),
+  SPRITE: Number(rawProductList[3][2]),
+  WATER: Number(rawProductList[6][2]),
+  VITAMIN_WATER: Number(rawProductList[7][2]),
+  POTATO_CHIP: Number(rawProductList[9][2]),
+  CHOCOLATE_BAR: Number(rawProductList[11][2]),
+  ENERGY_BAR: Number(rawProductList[12][2]),
+  PACKED_LUNCH: Number(rawProductList[13][2]),
+  CUP_RAMEN: Number(rawProductList[15][2]),
 };
 
 const PROMOTION_PRODUCT_AMOUNT = {
-  COKE: Number(productAmount[0][2]),
-  SPRITE: Number(productAmount[2][2]),
-  ORANGE_JUICE: Number(productAmount[4][2]),
-  SPARKLING_WATER: Number(productAmount[5][2]),
-  POTATO_CHIP: Number(productAmount[8][2]),
-  CHOCOLATE_BAR: Number(productAmount[10][2]),
-  CUP_RAMEN: Number(productAmount[14][2]),
+  COKE: Number(rawProductList[0][2]),
+  SPRITE: Number(rawProductList[2][2]),
+  ORANGE_JUICE: Number(rawProductList[4][2]),
+  SPARKLING_WATER: Number(rawProductList[5][2]),
+  POTATO_CHIP: Number(rawProductList[8][2]),
+  CHOCOLATE_BAR: Number(rawProductList[10][2]),
+  CUP_RAMEN: Number(rawProductList[14][2]),
 };
 
 const TOTAL_PRODUCT_AMOUNT = {
-  COKE: Number(productAmount[1][2]) + Number(productAmount[0][2]),
-  SPRITE: Number(productAmount[3][2]) + Number(productAmount[2][2]),
-  WATER: Number(productAmount[6][2]),
-  VITAMIN_WATER: Number(productAmount[7][2]),
-  POTATO_CHIP: Number(productAmount[9][2]) + Number(productAmount[8][2]),
-  CHOCOLATE_BAR: Number(productAmount[11][2]) + Number(productAmount[10][2]),
-  ENERGY_BAR: Number(productAmount[12][2]),
-  PACKED_LUNCH: Number(productAmount[13][2]),
-  CUP_RAMEN: Number(productAmount[15][2]) + Number(productAmount[14][2]),
+  COKE: Number(rawProductList[1][2]) + Number(rawProductList[0][2]),
+  SPRITE: Number(rawProductList[3][2]) + Number(rawProductList[2][2]),
+  WATER: Number(rawProductList[6][2]),
+  VITAMIN_WATER: Number(rawProductList[7][2]),
+  POTATO_CHIP: Number(rawProductList[9][2]) + Number(rawProductList[8][2]),
+  CHOCOLATE_BAR: Number(rawProductList[11][2]) + Number(rawProductList[10][2]),
+  ENERGY_BAR: Number(rawProductList[12][2]),
+  PACKED_LUNCH: Number(rawProductList[13][2]),
+  CUP_RAMEN: Number(rawProductList[15][2]) + Number(rawProductList[14][2]),
 };
 
 // 사용자가 최종적으로 받는 수량
@@ -93,4 +107,5 @@ export {
   NO_PROMOTION_PRODUCTS_NAME_LIST,
   USER_RECEIVED_PRODUCT_AMOUNT,
   TOTAL_PRODUCTS_NAME_LIST,
+  PRODUCTS
 };
--- src/View/outputViews.js
+++ src/View/outputViews.js
@@ -1,12 +1,7 @@
 import { Console } from '@woowacourse/mission-utils';
 import { ParedProductsData } from '../Model/productsInPossessionData.js';
 import { OUTPUT_MESSAGE } from '../Constant/messages.js';
-import {
-  NORMAL_PRODUCT_AMOUNT,
-  PROMOTION_PRODUCT_AMOUNT,
-  PROMOTION_PRODUCTS_NAME_LIST,
-  TOTAL_PRODUCT_AMOUNT,
-} from '../Constant/productsCount.js';
+import { PRODUCTS } from '../Constant/productsCount.js';
 
 export class Output {
   async printProductsInPossessionList() {
@@ -14,38 +9,20 @@ export class Output {
 
     Console.print(OUTPUT_MESSAGE.WELCOME_GREETING);
 
-    Console.print(
-      `- ${rawProductsList[0][0]} 1,000원 ${PROMOTION_PRODUCT_AMOUNT.COKE}개 ${rawProductsList[0][3]}` +
-        '\n' +
-        `- ${rawProductsList[1][0]} 1,000원 ${NORMAL_PRODUCT_AMOUNT.COKE}개` +
-        '\n' +
-        `- ${rawProductsList[2][0]} 1,000원 ${PROMOTION_PRODUCT_AMOUNT.SPRITE}개 ${rawProductsList[2][3]}` +
-        '\n' +
-        `- ${rawProductsList[3][0]} 1,000원 ${NORMAL_PRODUCT_AMOUNT.SPRITE}개 ` +
-        '\n' +
-        `- ${rawProductsList[4][0]} 1,800원 ${PROMOTION_PRODUCT_AMOUNT.ORANGE_JUICE}개 ${rawProductsList[4][3]}` +
-        '\n' +
-        `- ${rawProductsList[5][0]} 1,200원 ${PROMOTION_PRODUCT_AMOUNT.SPARKLING_WATER}개 ${rawProductsList[5][3]}` +
-        '\n' +
-        `- ${rawProductsList[6][0]} 500원 ${NORMAL_PRODUCT_AMOUNT.WATER}개` +
-        '\n' +
-        `- ${rawProductsList[7][0]} 1,500원 ${NORMAL_PRODUCT_AMOUNT.VITAMIN_WATER}개 ` +
-        '\n' +
-        `- ${rawProductsList[8][0]} 1,500원 ${PROMOTION_PRODUCT_AMOUNT.POTATO_CHIP}개 ${rawProductsList[8][3]}` +
-        '\n' +
-        `- ${rawProductsList[9][0]} 1,500원 ${NORMAL_PRODUCT_AMOUNT.POTATO_CHIP}개` +
-        '\n' +
-        `- ${rawProductsList[10][0]} 1,200원 ${PROMOTION_PRODUCT_AMOUNT.CHOCOLATE_BAR}개 ${rawProductsList[10][3]}` +
-        '\n' +
-        `- ${rawProductsList[11][0]} 1,200원 ${NORMAL_PRODUCT_AMOUNT.CHOCOLATE_BAR}개` +
-        '\n' +
-        `- ${rawProductsList[12][0]} 2,000원 ${NORMAL_PRODUCT_AMOUNT.ENERGY_BAR}개` +
-        '\n' +
-        `- ${rawProductsList[13][0]} 6,400원 ${NORMAL_PRODUCT_AMOUNT.PACKED_LUNCH}개` +
-        '\n' +
-        `- ${rawProductsList[14][0]} 1,700원 ${PROMOTION_PRODUCT_AMOUNT.CUP_RAMEN}개 ${rawProductsList[14][3]}` +
-        '\n' +
-        `- ${rawProductsList[15][0]} 1,700원 ${NORMAL_PRODUCT_AMOUNT.CUP_RAMEN}개`
-    );
+    PRODUCTS.forEach((product) => {
+      // 프로모션 재고가 있을 경우 출력
+      if (product.promotionStock) {
+        Console.print(
+          `${product.productName} ${product.price}원 ${product.promotionStock}개 ${product.promotionType}`
+        );
+      }
+
+      // 일반 재고가 있을 경우 출력
+      if (product.regularStock) {
+        Console.print(
+          `${product.productName} ${product.price}원 ${product.regularStock}개`
+        );
+      }
+    });
   }
 }
