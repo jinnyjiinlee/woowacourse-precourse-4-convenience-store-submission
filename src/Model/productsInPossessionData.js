9c9a2cb58275921bdaad7687c5319a368e8a5ef9
Jinny <ggab.challenge@gmail.com>
2024-11-08 21:00:20 +0900

    refactor: 사용자에게 보여주는 상품 목록, 행사 목록을 products.md파일에서 직접 불러오기

--- src/Model/productsInPossessionData.js
+++ src/Model/productsInPossessionData.js
@@ -2,35 +2,16 @@ import fs from 'fs';
 
 export class ParedProductsData {
   constructor() {
-    this.product = null;
-    this.price = null;
-    this.amount = null;
-    this.promotion = null;
-
-    this.finalProductsInPossessionList = [];
     this.rawProductsList = [];
-    this.productsRawData = null;
-    this.lineSeparatedProductsData = null;
-  }
-
-  generateProductPrintList() {
-    this.productsInPossession();
-
-    return this.finalProductsInPossessionList;
   }
 
   productsInPossession = () => {
     this.readProductsRawData();
     this.parseProductLine();
-
     for (let i = 1; i < 17; i += 1) {
       this.eachCategory = this.lineSeparatedProductsData[i].split(',');
-      this.formatProductsData();
       this.rawProductsList.push(this.eachCategory);
-      const resultEachLine = `- ${this.product} ${this.price}원 ${this.amount}개 ${this.promotion}`;
-      this.finalProductsInPossessionList.push(resultEachLine);
     }
-    console.log(this.rawProductsList);
 
     return this.rawProductsList;
   };
@@ -44,18 +25,5 @@ export class ParedProductsData {
 
   parseProductLine = () => {
     this.lineSeparatedProductsData = this.productsRawData.split('\n');
-  };
-
-  formatProductsData = () => {
-    this.product = this.eachCategory[0];
-    this.price = this.eachCategory[1]
-      .toString()
-      .replace(/\B(?=(\d{3})+(?!\d))/g, ',');
-    this.amount = this.eachCategory[2];
-    this.promotion = this.eachCategory[3];
-
-    if (this.promotion === 'null') {
-      this.promotion = '';
-    }
   };
 }
--- src/View/outputViews.js
+++ src/View/outputViews.js
@@ -1,19 +1,51 @@
 import { Console } from '@woowacourse/mission-utils';
 import { ParedProductsData } from '../Model/productsInPossessionData.js';
 import { OUTPUT_MESSAGE } from '../Constant/messages.js';
+import {
+  NORMAL_PRODUCT_AMOUNT,
+  PROMOTION_PRODUCT_AMOUNT,
+  PROMOTION_PRODUCTS_NAME_LIST,
+  TOTAL_PRODUCT_AMOUNT,
+} from '../Constant/productsCount.js';
 
 export class Output {
-  constructor() {
-    this.productPrintList = [];
-  }
-
   async printProductsInPossessionList() {
-    this.productPrintList = new ParedProductsData().generateProductPrintList();
+    const rawProductsList = new ParedProductsData().productsInPossession();
 
     Console.print(OUTPUT_MESSAGE.WELCOME_GREETING);
 
-    for (let i = 0; i < 16; i += 1) {
-      Console.print(this.productPrintList[i]);
-    }
+    Console.print(
+      `- ${rawProductsList[0][0]} 1,000원 ${PROMOTION_PRODUCT_AMOUNT.COKE}개 ${rawProductsList[0][3]}` +
+        '\n' +
+        `- ${rawProductsList[1][0]} 1,000원 ${NORMAL_PRODUCT_AMOUNT.COKE}개` +
+        '\n' +
+        `- ${rawProductsList[2][0]} 1,000원 ${PROMOTION_PRODUCT_AMOUNT.SPRITE}개 ${rawProductsList[2][3]}` +
+        '\n' +
+        `- ${rawProductsList[3][0]} 1,000원 ${NORMAL_PRODUCT_AMOUNT.SPRITE}개 ` +
+        '\n' +
+        `- ${rawProductsList[4][0]} 1,800원 ${PROMOTION_PRODUCT_AMOUNT.ORANGE_JUICE}개 ${rawProductsList[4][3]}` +
+        '\n' +
+        `- ${rawProductsList[5][0]} 1,200원 ${PROMOTION_PRODUCT_AMOUNT.SPARKLING_WATER}개 ${rawProductsList[5][3]}` +
+        '\n' +
+        `- ${rawProductsList[6][0]} 500원 ${NORMAL_PRODUCT_AMOUNT.WATER}개` +
+        '\n' +
+        `- ${rawProductsList[7][0]} 1,500원 ${NORMAL_PRODUCT_AMOUNT.VITAMIN_WATER}개 ` +
+        '\n' +
+        `- ${rawProductsList[8][0]} 1,500원 ${PROMOTION_PRODUCT_AMOUNT.POTATO_CHIP}개 ${rawProductsList[8][3]}` +
+        '\n' +
+        `- ${rawProductsList[9][0]} 1,500원 ${NORMAL_PRODUCT_AMOUNT.POTATO_CHIP}개` +
+        '\n' +
+        `- ${rawProductsList[10][0]} 1,200원 ${PROMOTION_PRODUCT_AMOUNT.CHOCOLATE_BAR}개 ${rawProductsList[10][3]}` +
+        '\n' +
+        `- ${rawProductsList[11][0]} 1,200원 ${NORMAL_PRODUCT_AMOUNT.CHOCOLATE_BAR}개` +
+        '\n' +
+        `- ${rawProductsList[12][0]} 2,000원 ${NORMAL_PRODUCT_AMOUNT.ENERGY_BAR}개` +
+        '\n' +
+        `- ${rawProductsList[13][0]} 6,400원 ${NORMAL_PRODUCT_AMOUNT.PACKED_LUNCH}개` +
+        '\n' +
+        `- ${rawProductsList[14][0]} 1,700원 ${PROMOTION_PRODUCT_AMOUNT.CUP_RAMEN}개 ${rawProductsList[14][3]}` +
+        '\n' +
+        `- ${rawProductsList[15][0]} 1,700원 ${NORMAL_PRODUCT_AMOUNT.CUP_RAMEN}개`
+    );
   }
 }
