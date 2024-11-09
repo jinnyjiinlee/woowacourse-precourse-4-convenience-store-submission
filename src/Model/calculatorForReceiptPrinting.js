6420f0b1e64537ebfaf62e342107d5e509884b88
Jinny <ggab.challenge@gmail.com>
2024-11-09 20:07:47 +0900

    feat: 사용자가 구매한 프로모션 상품 수량과 가격이 행사할인 가격으로 영수증에 나오는 기능 구현

--- src/Model/calculatorForReceiptPrinting.js
+++ src/Model/calculatorForReceiptPrinting.js
@@ -3,22 +3,20 @@ import { PRODUCTS } from '../Constant/productsCount.js';
 
 export class receiptPrinting {
   printReceipt() {
+    // 구매한 상품명
     const purchasedProductName = PRODUCTS.filter(
       (product) => product.totalReceivedAmount > 0
     ).map((product) => product.productName);
 
+    // 구매한 상품 개수
     const purchasedProductAmount = PRODUCTS.filter(
       (product) => product.totalReceivedAmount > 0
     ).map((product) => product.totalReceivedAmount);
 
+    // 구매한 상품 가격
     const purchasedProductPrice = PRODUCTS.filter(
       (product) => product.totalReceivedAmount > 0
     ).map((product) => product.price);
-
-    // console.clear();
-    // console.log('purchasedProductName: ', purchasedProductName);
-    // console.log('purchasedProductAmount: ', purchasedProductAmount);
-    // console.log('purchasedProductPrice: ', purchasedProductPrice);
 
     Console.print('==============W 편의점================');
     Console.print('상품명               수량         금액  ');
@@ -44,16 +42,37 @@ export class receiptPrinting {
       );
     }
 
+    // 프로모션 상품명
     const giftPurchasedProductName = PRODUCTS.filter(
       (product) => product.receivedGiftAmount > 0
     ).map((product) => product.productName);
 
+    // 프로모션 개수
     const giftPurchasedProductAmount = PRODUCTS.filter(
       (product) => product.receivedGiftAmount > 0
     ).map((product) => product.receivedGiftAmount);
 
+    console.log('giftPurchasedProductAmount: ', giftPurchasedProductAmount)
+
+    // 프로모션 가격
+    const giftPurchasedProductPrice = PRODUCTS.filter(
+      (product) => product.receivedGiftAmount > 0
+    ).map((product) => product.price);
+
+    console.log('giftPurchasedProductPrice: ', giftPurchasedProductPrice)
+
+    // 프로모션 상품 총 구입 금액
+    let totalGiftPurchasedProductPrice = 0;
+
+    for (let i = 0; i < giftPurchasedProductName.length; i += 1) {
+      totalGiftPurchasedProductPrice +=
+        giftPurchasedProductAmount[i] * giftPurchasedProductPrice[i];
+        console.log('totalGiftPurchasedProductPrice: ', totalGiftPurchasedProductPrice)
+    }
+
     Console.print('\n' + '=============증      정===============');
 
+    // 프로모션 상품, 개수
     for (let i = 0; i < giftPurchasedProductName.length; i += 1) {
       Console.print(
         `${giftPurchasedProductName[i].padEnd(10, 'ㅡ')} ${String(
@@ -61,20 +80,20 @@ export class receiptPrinting {
         ).padEnd(1, ' ')}`
       );
     }
+
     Console.print('\n' + '=====================================');
+    // 총 수량, 총 금액
     Console.print(
-      `총구매액 ${String(totalPurchasedProductAmount).padStart(13)} ${String(totalPurchasedProductPrice).padStart(14)}`
+      `총구매액 ${String(totalPurchasedProductAmount).padStart(13)} ${String(
+        totalPurchasedProductPrice
+      ).padStart(14)}`
     );
-    Console.print(`행사할인                     -{(13, 000)}`);
+    // 행사할인 금액
+    // TODO: 나중에 간격 맞추기
+    Console.print(`행사할인                        -${totalGiftPurchasedProductPrice}`);
     Console.print(`맴버십할인                   {(13, 000)}`);
     Console.print(`내실돈                       {(13, 000)}`);
   }
 }
-
-// Console.print('====================================');
-// Console.print(`총구매액             ${8}     ${(13, 000)}`);
-// Console.print(`행사할인                     -${(13, 000)}`);
-// Console.print(`맴버십할인                    -${(13, 000)}`);
-// Console.print(`내실돈                       ${(13, 000)}`);
 
 new receiptPrinting().printReceipt();
