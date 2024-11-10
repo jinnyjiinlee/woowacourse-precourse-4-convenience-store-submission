a7c372cddff42f7d4f5b2ebafc34c9e351435cb4
Jinny <ggab.challenge@gmail.com>
2024-11-10 17:34:13 +0900

    refactor: Output클래스 각 함수 10줄로 변경

--- src/View/outputViews.js
+++ src/View/outputViews.js
@@ -1,39 +1,54 @@
 import { Console } from '@woowacourse/mission-utils';
-import { ParedProductsData } from '../Model/productsInPossessionData.js';
 import { OUTPUT_MESSAGE } from '../Constant/messages.js';
 import { PRODUCTS } from '../Constant/productsCount.js';
 
 export class Output {
   async printProductsInPossessionList() {
-    const rawProductsList = new ParedProductsData().productsInPossession();
-
     Console.print(OUTPUT_MESSAGE.WELCOME_GREETING);
 
     PRODUCTS.forEach((product) => {
-      // 프로모션 재고 출력
-      if (product.promotionStock !== null) {
-        let promotionStockText = `${product.promotionStock}개 ${
-          product.promotionType || ''}`;
-        if (product.promotionStock === 0) {
-          promotionStockText = '재고 없음';
-        }
-
-        Console.print(
-          `- ${product.productName} ${product.price.toLocaleString()}원 ${promotionStockText}`
-        );
-      }
+      this.product = product;
+      // 프로모션 재고 확인 - 출력
+      this.promotionStockChecking();
 
       // 일반 재고 출력
-      if (product.regularStock !== null) {
-        let regularStockText = `${product.regularStock}개`;
-        if (product.regularStock === 0) {
-          regularStockText = '재고 없음';
-        }
-
-        Console.print(
-          `- ${product.productName} ${product.price.toLocaleString()}원 ${regularStockText}`
-        );
-      }
+      this.regularStockChecking();
     });
+  }
+
+  promotionStockChecking() {
+    if (this.product.promotionStock !== null) {
+      this.promotionStockText = `${this.product.promotionStock}개 ${
+        this.product.promotionType || ''}`;
+      if (this.product.promotionStock === 0) {
+        this.printProductsInPossessionList.this.promotionStockText = '재고 없음';
+      }
+      this.promotionStockPrint();
+    }
+  }
+
+  regularStockChecking() {
+    if (this.product.regularStock !== null) {
+      this.regularStockText = `${this.product.regularStock}개`;
+      if (this.product.regularStock === 0) {
+        this.regularStockText = '재고 없음';}
+      this.regularStockPrint();
+    }
+  }
+
+  promotionStockPrint() {
+    Console.print(
+      `- ${this.product.productName} ${this.product.price.toLocaleString()}원 ${
+        this.promotionStockText
+      }`
+    );
+  }
+
+  regularStockPrint() {
+    Console.print(
+      `- ${this.product.productName} ${this.product.price.toLocaleString()}원 ${
+        this.regularStockText
+      }`
+    );
   }
 }
