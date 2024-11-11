b3eb312c3d850924f55898bc3564ddc77c1a135c
Jinny <ggab.challenge@gmail.com>
2024-11-11 19:24:49 +0900

    fix: 상품 리스트 개수, 일반 재고, 이벤트 재고 수량, 이벤트 타입 md파일에서 들고오기

--- src/Constant/productList.js
+++ src/Constant/productList.js
@@ -2,52 +2,51 @@ import { ParedProductsData } from '../Model/productsInPossessionData.js';
 
 const rawProductList = new ParedProductsData().productsInPossession();
 
-// TODO: productName이외 파일도 md파일에서 들고오기
 const PRODUCTS = [
   {
     productName: rawProductList[0][0],
-    price: 1000,
-    regularStock: 10,
-    promotionStock: 10,
-    initialPromotionStock: 10,
-    promotionType: '탄산2+1',
+    price: Number(rawProductList[0][1]),
+    regularStock: Number(rawProductList[1][2]),
+    promotionStock: Number(rawProductList[0][2]),
+    initialPromotionStock: Number(rawProductList[0][2]),
+    promotionType: rawProductList[0][3],
     totalReceivedQuantities: 0,
     receivedGiftQuantities: 0,
   }, // 콜라 - 2+1
   {
     productName: rawProductList[2][0],
-    price: 1000,
-    regularStock: 7,
-    promotionStock: 8,
-    initialPromotionStock: 8,
-    promotionType: '탄산2+1',
+    price: Number(rawProductList[2][1]),
+    regularStock: Number(rawProductList[3][2]),
+    promotionStock: Number(rawProductList[2][2]),
+    initialPromotionStock: Number(rawProductList[2][2]),
+    promotionType: rawProductList[2][3],
     totalReceivedQuantities: 0,
     receivedGiftQuantities: 0,
   }, // 사이다 - 2+1
   {
     productName: rawProductList[4][0],
-    price: 1800,
+    price: Number(rawProductList[4][1]),
     regularStock: 0,
-    promotionStock: 9,
-    initialPromotionStock: 9,
-    promotionType: 'MD추천상품',
+    promotionStock: Number(rawProductList[4][2]),
+    initialPromotionStock: Number(rawProductList[4][2]),
+    promotionType: rawProductList[4][3],
     totalReceivedQuantities: 0,
     receivedGiftQuantities: 0,
   }, // 오렌지주스 = 1+1
   {
     productName: rawProductList[5][0],
-    price: 1200,
+    price: Number(rawProductList[5][1]),
     regularStock: 0,
-    promotionStock: 5,
-    initialPromotionStock: 5,
-    promotionType: '탄산2+1',
+    promotionStock: Number(rawProductList[5][2]),
+    initialPromotionStock: Number(rawProductList[5][2]),
+    promotionType: rawProductList[5][3],
     totalReceivedQuantities: 0,
     receivedGiftQuantities: 0,
   }, // 탄산수 - 2+1
   {
     productName: rawProductList[6][0],
-    price: 500,
-    regularStock: 10,
+    price: Number(rawProductList[6][1]),
+    regularStock: Number(rawProductList[6][2]),
     promotionStock: null,
     initialPromotionStock: null,
     promotionType: null,
@@ -56,8 +55,8 @@ const PRODUCTS = [
   }, // 물
   {
     productName: rawProductList[7][0],
-    price: 1500,
-    regularStock: 6,
+    price: Number(rawProductList[7][1]),
+    regularStock: Number(rawProductList[7][2]),
     promotionStock: null,
     initialPromotionStock: null,
     promotionType: null,
@@ -66,38 +65,39 @@ const PRODUCTS = [
   }, // 비타민워터
   {
     productName: rawProductList[8][0],
-    price: 1500,
-    regularStock: 5,
-    promotionStock: 5,
-    initialPromotionStock: 5,
-    promotionType: '반짝할인',
+    price: Number(rawProductList[8][1]),
+    regularStock: Number(rawProductList[9][2]),
+    promotionStock: Number(rawProductList[8][2]),
+    initialPromotionStock: Number(rawProductList[8][2]),
+    promotionType: rawProductList[8][3],
     totalReceivedQuantities: 0,
     receivedGiftQuantities: 0,
   }, // 감자칩 1+1
   {
     productName: rawProductList[10][0],
-    price: 1200,
-    regularStock: 5,
-    promotionStock: 5,
-    initialPromotionStock: 5,
-    promotionType: 'MD추천상품',
+    price: Number(rawProductList[10][1]),
+    regularStock: Number(rawProductList[11][2]),
+    promotionStock: Number(rawProductList[10][2]),
+    initialPromotionStock: Number(rawProductList[10][2]),
+    promotionType: rawProductList[10][3],
     totalReceivedQuantities: 0,
     receivedGiftQuantities: 0,
   }, //초코바 1+1
   {
     productName: rawProductList[12][0],
-    price: 2000,
-    regularStock: 5,
+    price: Number(rawProductList[12][1]),
+    regularStock: Number(rawProductList[12][2]),
     promotionStock: null,
     initialPromotionStock: null,
     promotionType: null,
     totalReceivedQuantities: 0,
     receivedGiftQuantities: 0,
   }, // 에너지바
+
   {
     productName: rawProductList[13][0],
-    price: 6400,
-    regularStock: 8,
+    price: Number(rawProductList[13][1]),
+    regularStock: Number(rawProductList[13][2]),
     promotionStock: null,
     initialPromotionStock: null,
     promotionType: null,
@@ -106,11 +106,11 @@ const PRODUCTS = [
   }, // 정식도시락
   {
     productName: rawProductList[14][0],
-    price: 1700,
-    regularStock: 10,
-    promotionStock: 1,
-    initialPromotionStock: 1,
-    promotionType: 'MD추천상품',
+    price: Number(rawProductList[14][1]),
+    regularStock: Number(rawProductList[15][2]),
+    promotionStock: Number(rawProductList[14][2]),
+    initialPromotionStock: Number(rawProductList[14][2]),
+    promotionType: rawProductList[14][3],
     totalReceivedQuantities: 0,
     receivedGiftQuantities: 0,
   }, //컵라면 1+1
